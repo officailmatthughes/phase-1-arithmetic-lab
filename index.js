829373db197d76642bddfc55d1d5dadaa5029c89
Mattherw <officialmatthughes>
2022-05-24 17:26:29 -0700

    commit tree 60171aff387678b60025789afb2eef548b1174da

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2;
+let num2 = 31
 
+const multiply = num1 * num2
+
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 40;
+let num4= 10
+const mod = num3 / num4
+
+const max = Math.max(10, 15, 20)
 // Write your code here
